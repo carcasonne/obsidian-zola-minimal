e5f9cd1114d0467ac3565a3288faa871257b054b
carcasonne <villumsonne@protonmail.com>
2025-11-01 16:50:57 +0100

    Add tmux script to quickly set up dev env :-)

---
+++ tmux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+
+SESSION="obsidian-zola"
+
+tmux has-session -t $SESSION 2>/dev/null && tmux kill-session -t $SESSION
+
+tmux new-session -d -s $SESSION -n "zola"
+tmux send-keys -t $SESSION:0 ". ./env.sh && zola --root=build serve" C-m
+
+tmux new-window -t $SESSION:1 -n "ide"
+tmux send-keys -t $SESSION:1 "code ." C-m
+
+tmux new-window -t $SESSION:2 -n "watch"
+tmux send-keys -t $SESSION:2 ". ./env.sh && find zola -type f | entr -r ./build.sh" C-m
+
+tmux select-window -t $SESSION:2
+
+tmux attach -t $SESSION
